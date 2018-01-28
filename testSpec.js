3f2ab099d598f0ce23dfcf661997200971f74315
AP-MEDIA\dallas.gale-la <dallasgale@hotmail.com>
2018-01-29 10:59:14 +1100

    currency formatting tests added

--- testSpec.js
+++ testSpec.js
@@ -41,24 +41,60 @@ function convertInchToCm(inch) {
   return cm;
 }
 
+describe("distance converter", function() {
+  it("converts inches to centimeters", function() {
+    // expect(Convert(12, "in").to("cm")).toEqual(30.48);
+    const inch = convertInchToCm(12);
+    expect(inch).toEqual(30.48);
+  });
+});
+
+
+//////////// measurement converter: example 2 ////////////
+
+
 function convertLitreToGallon(litre) {
   var gallon = litre * 0.26417;
   return gallon;
 }
 
-describe("Convert library", function() {
-  describe("diistance converter", function() {
-    it("converts inches to centimeters", function() {
-      // expect(Convert(12, "in").to("cm")).toEqual(30.48);
-      const inch = convertInchToCm(12);
-      expect(inch).toEqual(30.48);
-    });
-  });
-  describe("volume converter", function() {
-    it("converts litres to gallons", function() {
-      // expect(Convert(3, "litres").to("gallons")).toEqual(0.79251);
-      const litre = convertLitreToGallon(3);
-      expect(litre).toEqual(0.79251);
-    })
+describe("volume converter", function() {
+  it("converts litres to gallons", function() {
+    // expect(Convert(3, "litres").to("gallons")).toEqual(0.79251);
+    const litre = convertLitreToGallon(3);
+    expect(litre).toEqual(0.79251);
+  })
+});
+
+
+//////////// currency formater: example 1 ////////////
+
+function currencyFormat(price) {
+   const uniquePrice = new Intl.NumberFormat().format(price);
+   return uniquePrice;
+}
+
+describe("currency formater", function() {
+  it("converts raw number to US currency format", function() {
+    const number = currencyFormat(328567);
+    expect(number).toEqual("328,567");
   });
 });
+
+
+//////////// currency formater: example 2 ////////////
+
+
+function addDollarSign(number) {
+   const rawNumber = number.toString();
+   const dollarSignedNumber = "$" + rawNumber;
+   return dollarSignedNumber;
+}
+
+describe("dollar sign", function() {
+  it("adds a dollar sign to the start of currency formatted number", function() {
+     const number = addDollarSign(currencyFormat(345325));
+     console.log(number);
+     expect(number).toEqual("$345,325");
+  });
+})
